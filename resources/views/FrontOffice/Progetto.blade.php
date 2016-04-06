04e0b6487e9dbf64df9e159b79f285c0dc860cd5
Davide Frasca <davide.frasca.stage@digicamere.it>
2016-04-06 10:07:19 +0200

    Added table in Progetto.blade.php

--- resources/views/FrontOffice/Progetto.blade.php
+++ resources/views/FrontOffice/Progetto.blade.php
@@ -4,6 +4,16 @@
 
     <p>
         <h1>Il Progetto</h1>
+        <table>
+            <tr>
+                <td>Riga 1</td>
+                <td>Value 1</td>
+            </tr>
+            <tr>
+                <td>Riga 2</td>
+                <td>Value 2</td>
+            </tr>
+        </table>
     </p>
 
 @stop
