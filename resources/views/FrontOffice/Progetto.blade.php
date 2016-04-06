0457654516e1e537ad74ab4e24d9231803fc41ce
Davide Frasca <davide.frasca.stage@digicamere.it>
2016-04-06 10:12:07 +0200

    Added inline styles to table of Progetto.blade.php

--- resources/views/FrontOffice/Progetto.blade.php
+++ resources/views/FrontOffice/Progetto.blade.php
@@ -4,7 +4,7 @@
 
     <p>
         <h1>Il Progetto</h1>
-        <table>
+        <table border="1" style="border: 1px solid #999; border-spacing: 0px;">
             <tr>
                 <td>Riga 1</td>
                 <td>Value 1</td>
